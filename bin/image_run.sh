1375780b557b53aba2c44ce8dd7e532acfd4823a
deploys <deploys@lyft.com>
2020-12-10 16:17:37 -0800

    support local images

--- bin/image_run.sh
+++ bin/image_run.sh
@@ -6,16 +6,20 @@ IMAGE_SHA=$2
 REGISTRY_ID=173840052742
 IMAGE_PATH=$REGISTRY_ID.dkr.ecr.us-east-1.amazonaws.com/$IMAGE_NAME:$IMAGE_SHA
 
-# auth
-source $HOME/ws/git/awsaccess/oktaawsaccess.sh
-oktaawsaccess clear && oktaawsaccess zimride-developer
-`aws ecr get-login --no-include-email --registry-ids $REGISTRY_ID`
+if [ -z "$IMAGE_SHA" ]; then
+  IMAGE_ID=$IMAGE_NAME
+else
+  # auth
+  source $HOME/ws/git/awsaccess/oktaawsaccess.sh
+  oktaawsaccess clear && oktaawsaccess zimride-developer
+  `aws ecr get-login --no-include-email --registry-ids $REGISTRY_ID`
 
-# fetch
-docker pull $IMAGE_PATH
+  # fetch
+  docker pull $IMAGE_PATH
 
-# find image and run
-IMAGE_ID=`docker images --format "{{.ID}}\t{{.Repository}}:{{.Tag}}" | grep $IMAGE_PATH | cut -f 1`
+  # find image and run
+  IMAGE_ID=`docker images --format "{{.ID}}\t{{.Repository}}:{{.Tag}}" | grep $IMAGE_PATH | cut -f 1`
+fi
 
 # To enter as root, add -u 0 to this command
 #DEBUG="-p 5005:5005"

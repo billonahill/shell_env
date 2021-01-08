9631e518ac2052e3464068877e8b6ea3dd2f6fd2
deploys <deploys@lyft.com>
2021-01-08 10:23:14 -0800

    Upgrade to 2.x

--- bin/image_run.sh
+++ bin/image_run.sh
@@ -4,7 +4,8 @@ set -x
 IMAGE_NAME=$1
 IMAGE_SHA=$2
 REGISTRY_ID=173840052742
-IMAGE_PATH=$REGISTRY_ID.dkr.ecr.us-east-1.amazonaws.com/$IMAGE_NAME:$IMAGE_SHA
+ECR_HOST=$REGISTRY_ID.dkr.ecr.us-east-1.amazonaws.com
+IMAGE_PATH=$ECR_HOST/$IMAGE_NAME:$IMAGE_SHA
 
 if [ -z "$IMAGE_SHA" ]; then
   IMAGE_ID=$IMAGE_NAME
@@ -12,7 +13,13 @@ else
   # auth
   source $HOME/ws/git/awsaccess/oktaawsaccess.sh
   oktaawsaccess clear && oktaawsaccess zimride-developer
-  `aws ecr get-login --no-include-email --registry-ids $REGISTRY_ID`
+
+  # If using aws CLI version 1.*:
+  #`aws ecr get-login --no-include-email --registry-ids $REGISTRY_ID`
+
+  # If using aws CLI version 2.*:
+  aws ecr get-login-password --region us-east-1 | \
+    docker login --username AWS  --password-stdin $ECR_HOST
 
   # fetch
   docker pull $IMAGE_PATH
